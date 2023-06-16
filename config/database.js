4158a5b18adb66c8fffc7e93e6910e9bac50d436
MehulExplorer <mehul.r@techladd.com>
2023-06-16 17:17:25 +0530

    updated db details

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "blog"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Mehul@1234"),
+      password: env("DATABASE_PASSWORD", "Postgres"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       // ssl: {
       //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
